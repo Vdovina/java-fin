61e53bd707c63b8775f6058da30cbe4e475bdf37
Fokina Viktoriya <v.fokina@greendatasoft.ru>
2021-06-22 20:28:22 +0500

    wtf?

--- src/main/java/com/petshop/javafin/config/MongoConfig.java
+++ src/main/java/com/petshop/javafin/config/MongoConfig.java
@@ -1,27 +1,29 @@
-//package com.petshop.javafin.config;
-//
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoClients;
-//import com.mongodb.client.internal.MongoClientImpl;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-//
-//public class MongoConfig extends AbstractMongoClientConfiguration {
-//
-//    @Value("${spring.data.mongodb.host}")
-//    private String host;
-//
-//    @Value("${spring.data.mongodb.database}")
-//    private  String database;
-//
-//    @Override
-//    public MongoClient mongoClient(){
-//        return new MongoClients.create();
-//    }
-//
-//
-//    @Override
-//    protected String getDatabaseName() {
-//        return database;
-//    }
-//}
+package com.petshop.javafin.config;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.database}")
+    private  String database;
+
+    @Value("${spring.data.mongodb.uri}")
+    private  String connectionString;
+
+    @Override
+    public MongoClient mongoClient(){
+        return new MongoClients.create(connectionString);
+    }
+
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+}
